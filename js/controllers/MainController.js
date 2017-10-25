90ed2c3b2a2956dd2234724b44d142eeba655ce4
Alastoroo <colinblechet@yahoo.fr>
2017-10-25 16:18:54 +0200

    add photo test

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,7 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.presentation =
     {
-      image: 'http://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg',
+      image: 'img/photos/test3_FotorX.jpg'
       title: 'Présentation',
       info : 'Texte de présentation a ajouter'
     },
